d233bd8409062b779e43a877e6cea2b24dab8871
BeINone <sbeinone@gmail.com>
2018-12-16 15:57:56 +0900

    프로젝트 생성

---
+++ src/main/java/com/depromeet/yellowcardapi/YellowcardApiApplication.java
@@ -0,0 +1,14 @@
+package com.depromeet.yellowcardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YellowcardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YellowcardApiApplication.class, args);
+	}
+
+}
+
