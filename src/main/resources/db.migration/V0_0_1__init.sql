6dd8c9fe974c9190d92b2968253d78a4ebfb8ad2
BeINone <sbeinone@gmail.com>
2018-12-20 00:58:57 +0900

    DB 스키마 수정
    - 오타 수정, 관계 추가, 컬럼명 수정

--- src/main/resources/db.migration/V0_0_1__init.sql
+++ src/main/resources/db.migration/V0_0_1__init.sql
@@ -1,10 +1,10 @@
 CREATE TABLE user (
-  user_kakao_id BIGINT UNSIGNED NOT NULL,
+  user_id BIGINT UNSIGNED NOT NULL,
   name VARCHAR(20) NOT NULL,
   profile_image_url TEXT NULL,
-  tunmbnail_image_url TEXT NULL,
+  thumbnail_image_url TEXT NULL,
   description VARCHAR(100) NULL,
-  PRIMARY KEY (user_kakao_id)
+  PRIMARY KEY (user_id)
 );
 
 CREATE TABLE drink (
@@ -20,19 +20,22 @@ CREATE TABLE drink (
 
 CREATE TABLE user_drink (
   user_drink_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  user_kakao_id BIGINT UNSIGNED NOT NULL,
+  user_id BIGINT UNSIGNED NOT NULL,
   drink_type VARCHAR(20) NOT NULL,
   level TINYINT NOT NULL DEFAULT 1,
-  PRIMARY KEY (user_drink_id)
+  PRIMARY KEY (user_drink_id),
+  CONSTRAINT fk_user_drink_user_user_Id
+    FOREIGN KEY (user_id)
+    REFERENCES user (user_id),
 );
 
 CREATE TABLE drink_like (
-  user_kakao_id BIGINT UNSIGNED NOT NULL,
+  user_id BIGINT UNSIGNED NOT NULL,
   drink_id BIGINT UNSIGNED NOT NULL,
-  PRIMARY KEY (user_kakao_id, drink_id),
-  CONSTRAINT fk_drink_like_user_uesr_kakao_Id
-    FOREIGN KEY (user_kakao_id)
-    REFERENCES user (user_kakao_id),
+  PRIMARY KEY (user_id, drink_id),
+  CONSTRAINT fk_drink_like_user_user_Id
+    FOREIGN KEY (user_id)
+    REFERENCES user (user_id),
   CONSTRAINT fk_drink_like_drink_drink_id
     FOREIGN KEY (drink_id)
     REFERENCES drink (drink_id)
@@ -40,9 +43,12 @@ CREATE TABLE drink_like (
 
 CREATE TABLE drink_history (
   drink_history_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  user_kakao_id BIGINT UNSIGNED NOT NULL,
+  user_id BIGINT UNSIGNED NOT NULL,
   drink_type VARCHAR(20) NOT NULL,
   drink_capacity TINYINT NOT NULL DEFAULT 0,
   drunk_at DATE NOT NULL,
-  PRIMARY KEY (drink_history_id)
+  PRIMARY KEY (drink_history_id),
+  CONSTRAINT fk_drink_history_user_user_id
+    FOREIGN KEY (user_id)
+    REFERENCES drink (user_id)
 );
