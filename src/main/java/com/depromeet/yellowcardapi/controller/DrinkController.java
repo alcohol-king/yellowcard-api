b366cb1c61658b19c17e8aeff430a9e17cd03616
SoyoungPark <3457soso@naver.com>
2018-12-21 18:10:57 +0900

    url 매핑 작업 중

--- src/main/java/com/depromeet/yellowcardapi/controller/DrinkController.java
+++ src/main/java/com/depromeet/yellowcardapi/controller/DrinkController.java
@@ -5,8 +5,12 @@ import com.depromeet.yellowcardapi.dto.DrinkResponse;
 import com.depromeet.yellowcardapi.model.Drink;
 import com.depromeet.yellowcardapi.service.DrinkService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * RequiredArgsConstructor: final이나 NonNull인 필드 값만 파라미터로 받습니다.
@@ -21,6 +25,21 @@ public class DrinkController {
     public DrinkResponse createDrink(@RequestBody DrinkRequest drinkRequest) {
         Drink drink = drinkService.createDrink(drinkRequest.toDrink());
         return DrinkResponse.from(drink);
+    }
+
+    @GetMapping("/drinks")
+    @ResponseStatus(HttpStatus.OK)
+    public List<DrinkResponse> listDrink() {
+        return drinkService.listDrink().stream()
+                .map(DrinkResponse::from)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/drinks/{drink_id}")
+    @ResponseStatus(HttpStatus.OK)
+    public DrinkResponse getDrink(@PathVariable Integer drink_id) {
+        Drink drink = drinkService.getDrink(drink_id);
+        return DrinkResponse.from (drink);
     }
 
 }
