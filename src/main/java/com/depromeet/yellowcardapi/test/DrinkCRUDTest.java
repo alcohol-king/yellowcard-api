92e8dcf6cc77d2fe12de84b7a70f4c49cd0dc1d9
SoyoungPark <3457soso@naver.com>
2018-12-23 22:17:29 +0900

    테스트 코드 변경

--- src/main/java/com/depromeet/yellowcardapi/test/DrinkCRUDTest.java
+++ src/main/java/com/depromeet/yellowcardapi/test/DrinkCRUDTest.java
@@ -3,6 +3,7 @@ package com.depromeet.yellowcardapi.test;
 import com.depromeet.yellowcardapi.YellowcardApiApplication;
 import com.depromeet.yellowcardapi.domain.Drink;
 import com.depromeet.yellowcardapi.domain.DrinkRepository;
+import com.depromeet.yellowcardapi.service.DrinkService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,38 +11,49 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.Optional;
-
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = YellowcardApiApplication.class)
 public class DrinkCRUDTest {
     @Autowired private DrinkRepository drinkRepository;
+    @Autowired private DrinkService drinkService;
 
     private Drink drink;
 
     @Before
     public void setup() throws Exception {
         drink = new Drink();
-        drink.builder()
-                .name("밤막걸리")
-                .drink_type("막걸리")
-                .description("내가 좋아하는 막걸리")
-                .price(11)
-                .price(1000)
-                .build();
+        drink.setName("밤막걸리");
+        drink.setDrinkType("막걸리");
+        drink.setDescription("내가 좋아하는 막걸리");
+        drink.setProof(11);
+        drink.setPrice(1000);
 
-        drinkRepository.save(drink);
-        drinkRepository.flush();
+        drinkService.createDrink(drink);
     }
 
     @Test
     public void selectTest() throws Exception {
-        Optional<Drink> selectDrink = drinkRepository.findById(drink.getDrink_id());
-        assertEquals(selectDrink.get().getDrink_id(), drink.getDrink_id());
-        assertEquals(selectDrink.get().getName(), drink.getName());
-        assertNotNull(selectDrink.get().getName());
+        Drink selectedDrink = drinkRepository.findById(drink.getDrinkId()).get();
+        assertEquals(selectedDrink.getDrinkId(), drink.getDrinkId());
+        assertEquals(selectedDrink.getName(), drink.getName());
+        assertNotNull(selectedDrink.getName());
+    }
+
+    @Test
+    public void likeTest() throws Exception {
+        Drink selectedDrink = drinkRepository.findById(drink.getDrinkId()).get();
+        int beforeNumberOfLike = selectedDrink.getNumberOfLike();
+
+        drinkService.increaseLike(selectedDrink.getDrinkId());
+
+        selectedDrink = drinkRepository.findById(drink.getDrinkId()).get();
+        int afterNumberOfLike = selectedDrink.getNumberOfLike();
+
+        assertThat(afterNumberOfLike, is(beforeNumberOfLike+1));
     }
 }
