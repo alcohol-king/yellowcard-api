1d783eb00a22e6fcf1f6cb5d67e40051d4303039
SoyoungPark <3457soso@naver.com>
2018-12-26 20:03:19 +0900

    [WIP] 토큰 헤더로 주는 작업 진행중 (SwaggerConfig 변경)

--- src/main/java/com/depromeet/yellowcardapi/config/SwaggerConfig.java
+++ src/main/java/com/depromeet/yellowcardapi/config/SwaggerConfig.java
@@ -1,41 +1,101 @@
 package com.depromeet.yellowcardapi.config;
 
+import com.google.common.base.Predicates;
+import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.Parameter;
+import springfox.documentation.service.*;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurerAdapter {
 
     public static String DEFAULT_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNTQ1ODE5MzE4fQ.XhQV5F91vYB3W5a4cuIi9Io1w7Yt_FuxYUd_uwmE7V4";
 
+    @SuppressWarnings("unchecked")
     @Bean
-    public Docket api() {
-        ParameterBuilder parameterBuilder = new ParameterBuilder();
-        parameterBuilder.name("token")
-                        .parameterType("header")
-                        .defaultValue(DEFAULT_TOKEN)
-                        .modelRef(new ModelRef("string"))
-                        .required(true)
-                        .build();
-
-        ArrayList<Parameter> parameters = new ArrayList<>();
-
+    public Docket swaggerPlugin() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .globalOperationParameters(parameters)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.depromeet.yellowcardapi"))
                 .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.depromeet.yellowcardapi"))
+                .build().directModelSubstitute(LocalDate.class, String.class)
+                .genericModelSubstitutes(ResponseEntity.class)
+                .apiInfo(apiInfo())
+                .securitySchemes(Lists.newArrayList(apiKey()))
+                .securityContexts(Arrays.asList(securityContext()));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("yellocard-api")
+                .version("v0.0.1")
                 .build();
     }
+
+    @Bean
+    public SecurityConfiguration security() {
+        return new SecurityConfiguration(null, null, null,
+                null,
+                null,
+                ApiKeyVehicle.HEADER, AUTHORIZATION,
+                null);
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("apiKey", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope(
+                "global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("apiKey",
+                authorizationScopes));
+    }
+
+
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.depromeet.yellowcardapi"))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
 }
