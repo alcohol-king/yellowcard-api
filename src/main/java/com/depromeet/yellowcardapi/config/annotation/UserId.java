021d07fe94fd7ac170c1aa6a0e83ae997db99274
Cho Sungbin <sbeinone@gmail.com>
2018-12-27 12:25:58 +0900

    Revert "[WIP] #21 메인 화면 데이터 조회 API 추가"

--- src/main/java/com/depromeet/yellowcardapi/config/annotation/UserId.java
+++
@@ -1,9 +0,0 @@
-package com.depromeet.yellowcardapi.config.annotation;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.PARAMETER)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface UserId {
-}
