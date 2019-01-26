45cd40572bf5ab93c2b3a419672da13159d18088
BeINone <sbeinone@gmail.com>
2019-01-26 11:50:06 +0900

    [FIX] 미사용 import문 제거

--- src/main/java/com/depromeet/yellowcardapi/config/UserIdHandlerMethodArgumentResolver.java
+++ src/main/java/com/depromeet/yellowcardapi/config/UserIdHandlerMethodArgumentResolver.java
@@ -1,17 +1,8 @@
 package com.depromeet.yellowcardapi.config;
 
 import com.depromeet.yellowcardapi.config.annotation.UserId;
-import com.depromeet.yellowcardapi.service.JwtTokenProvider;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.MethodParameter;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.support.WebDataBinderFactory;
-import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.method.support.ModelAndViewContainer;
-
-import javax.servlet.http.HttpServletRequest;
 
 public abstract class UserIdHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
 
